30711bc34bd75245ad6764413f2ac9ffaa0fe8f2
FlaviaBertoni <fbbertoni.cit@rd.com.br>
2021-05-26 20:51:05 -0300

    Add mutations

--- api/user/dataSource/user.js
+++ api/user/dataSource/user.js
@@ -22,6 +22,29 @@ class UsersAPI extends RESTDataSource {
   async getRole(id) {
     return await this.get(`/roles/${id}`);
   }
+
+  async adicionaUser(user) {
+    const users = await this.getUsers();
+    const [role] = await this.get(`/roles?type=${user.role}`);
+    
+    user.id = users.length + 1;
+    await this.post('users', { ...user, role: role.id });
+
+    return ({ ...user, role})
+  }
+
+  async atualizaUser(user) {
+    const [role] = await this.get(`/roles?type=${user.role}`);
+    
+    await this.put(`users/${user.id}`, { ...user, role: role.id });
+
+    return ({ ...user, role})
+  }
+
+  async deletaUser(id) {    
+    await this.delete(`users/${id}`);
+    return id;
+  }
 }
 
 module.exports = UsersAPI
